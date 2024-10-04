b89cefabbe37c0fa4bcf3b0a8e92b0157fcead97
CristianRG <cristianrizogomez890@gmail.com>
2024-10-03 23:14:44 -0600

    updated: test file fixed to work when try to send mail and docummentation of this added

--- src/test/test.js
+++ src/test/test.js
@@ -6,10 +6,11 @@ const user = process.env.MAIL_SENDER
 const pass = process.env.PASS_SENDER
 
 const transporter = createTransport({ service: 'gmail', auth: { user: user, pass: pass } }) // Create a new transport to authenticate the sender
-const token = authenticateUser({ user: { mail: 'rizogomezalexander@gmail.com', password: '123456789' }, sender: user, transporter }) // Authenticate the user by mail sending a token
-
+const token = await authenticateUser({ user: { mail: 'receptoremail@domain.com', password: 'receptorpasswordinyourapp' }, sender: user, transporter }) // Authenticate the user by mail sending a token
 // Example
-// In case that receive a token in your app
+// In case that receive a token in your app. This will be true cause the dependence just show if your service accepted to send the message always 
+// receptor mail was wrong or not exits. Dont use this code above in production you can use this code inside a route in express where you receive a 
+// token as parameter
 const tokenReceived = new UserToken({ user: { email: null, password: null } }) // create a object
 tokenReceived.token = token // set token received
 const isAuthenticated = authenticateToken({ token: tokenReceived }) // check if token exits. Return true or false
