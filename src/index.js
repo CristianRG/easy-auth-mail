64822b48dab81f8e801acafc80d3320edc84524c
CristianRG <cristianrizogomez890@gmail.com>
2024-10-07 23:30:45 -0600

    update and docs: updated functions to work independently and added documentation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import nodemailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer/index.js'
 import StateToken from './models/StateToken.js'
-import UserToken from './models/UserToken.js'
 import fs from 'node:fs'
-import MailOptions from './models/MailOptions.js'
+import MailOptions, { types } from './models/MailOptions.js'
 
 const state = new StateToken()
 
 /**
- * Create a new transport to authenticate the sender
- * @param {string} service - Service of mail
- * @param {Object} auth - Authentication from sender. Use an object for example {user: 'youremail@gmail.com', pass: 'yourpassword'}
- * @returns {Mail} transporter 
+ * Crea un nuevo transportador para autenticar al remitente.
+ * @param {Object} param - Objecto con los parametros de servicio y autentificación del remitente
+ * @param {string} param.service - Servicio de correo (por ejemplo, 'gmail').
+ * @param {{user: string, pass: string}} param.auth - Objeto de autenticación del remitente, debe contener el correo y la contraseña { user: 'tuemail@gmail.com', pass: 'tupassword' }.
+ * @returns {Mail} transporter - Un objeto `transporter` para enviar correos.
  */
 export const createTransport = ({ service, auth }) => {
     const transporter = nodemailer.createTransport({
@@ -22,57 +22,64 @@ export const createTransport = ({ service, auth }) => {
 }
 
 /**
- * Authenticate a user by sending an email with their token.
+ * Crea las opciones de correo con el remitente, destinatario y contenido.
  * 
- * @param {Object} param - The object containing the user's information and email details.
- * @param {Object} param.user - The user object.
- * @param {string} param.user.mail - The user's email address where the authentication email will be sent.
- * @param {string} param.user.password - The user's password to generate the token
- * @param {string} param.sender - The email address of the sender.
- * @param {Mail} param.transporter - The Nodemailer transporter object used to send the email.
- * @param {Object} param.content
- * @param {String} param.content.subject
- * @param {String} param.content.type
- * @param {String} param.content.path
- * @returns {Promise} Returns a promise with token value
+ * @param {Object} param - Parámetros para crear las opciones de correo.
+ * @param {{mail: string}} param.sender - Correo del remitente.
+ * @param {{mail: string, token: string}} param.user - Correo y token del usuario.
+ * @param {{subject: string, document: {path: string, matches: [{match: { id: string, value: string }}]}}} param.content - Contenido del correo, incluyendo el asunto y documento HTML para personalización.
+ * @returns {MailOptions} - Objeto con las opciones configuradas para el correo.
  */
-export const authenticateUser = async ({ user: { mail, password }, sender, transporter, content: { subject, type, path } }) => {
-    const userToken = new UserToken({ user: { email: mail, password: password } })
-    userToken.generateToken()
+export const createMailOptions = async ({ sender, user, content }) => {
+    const mailOptions = new MailOptions(sender.mail, user.mail, content.subject)
 
-    const mailOptions = new MailOptions(sender, mail, subject)
-    let content = `Your token is ${userToken.token}`
-
-    if (path) {
-        const response = await customHTML({
-            path: path, matches: [
-                { match: { id: 'token', value: userToken.token } }, { match: { id: 'user', value: userToken.email } }
-            ]
-        })
-        if (response.type == 'success') content = response.data.html
+    if (content.document) {
+        const document = await customHTML({ path: content.document.path, matches: content.document.matches })
+        if (document.type == 'success') {
+            mailOptions.setContent(types['text/html'], document.data.html)
+        } else {
+            mailOptions.setContent(types['text/plain'], `Your token is ${user.token}`)
+        }
+        return mailOptions
     }
-    mailOptions.setContent(type, content)
 
-    const response = await transporter.sendMail(mailOptions.getObject())
+    mailOptions.setContent(types['text/plain'], `Your token is ${user.token}`)
+    return mailOptions
+}
+
+/**
+ * Autentica a un usuario enviando un correo con su token.
+ * 
+ * @param {Object} param - Objeto con la información del usuario y detalles del correo.
+ * @param {Object} param.userToken - Objeto de usuario que contiene el correo, token y contraseña
+ * @param {Mail} param.transport - Objeto transportador de Nodemailer para enviar el correo.
+ * @param {MailOptions} param.mailOptions - Opciones de correo configuradas para el envío.
+ * @param {Object} param.config - Objeto con configuraciones
+ * @param {number} param.config.timeOutDuration - Duración en milisegundos del token para expirar
+ * @returns {Promise<string>} - Retorna una promesa con el valor del token.
+ */
+export const authenticateUser = async ({ userToken, transport, mailOptions, config: { timeOutDuration } }) => {
+    const response = await transport.sendMail(mailOptions.getObject())
 
     return new Promise((resolve, reject) => {
         if (response.accepted.length > 0) {
             const timeOut = setTimeout(() => {
-                state.removeToken(userToken, (response) => { })
-            }, 10000)
+                state.removeToken(userToken, () => { })
+            }, timeOutDuration)
             state.addToken({ userToken, timeOutID: timeOut })
             resolve(userToken.token)
         } else {
-            reject('not send mail')
+            reject('Email no enviado')
         }
     })
 }
 
 /**
+ * Autentica el token de un usuario.
  * 
- * @param {Object} param - Receive a object with the value of token
- * @param {String} param.token - Value of token
- * @returns {Boolean}
+ * @param {Object} param - Objeto que contiene el valor del token.
+ * @param {string} param.token - Valor del token a autenticar.
+ * @returns {boolean} - Retorna `true` si el token es válido, de lo contrario `false`.
  */
 export const authenticateToken = ({ token }) => {
     const userToken = state.getToken(token)
@@ -86,12 +93,12 @@ export const authenticateToken = ({ token }) => {
 }
 
 /**
- * Set a custom HTML to send and set dinamyc values
- * @param {Object} param
- * @param {String} param.path
- * @param {String} param.token
- * @param {Array} param.matches - Set an array of objects like {match: {id: 'token', value: 'Token'}} to insert inner target html the value by id provided
- * @returns {Promise} - Promise with an object result. If is error indicates error. Otherwise return html data
+ * Personaliza un documento HTML para insertar valores dinámicos.
+ * 
+ * @param {Object} param - Parámetros para personalizar el HTML.
+ * @param {string} param.path - Ruta del archivo HTML.
+ * @param {Array} param.matches - Lista de objetos para hacer coincidencias con el HTML, { match: { id: 'token', value: 'Token' } }.
+ * @returns {Promise<{ type: string, message: string, data: { html: string, time: string } }>} - Promesa con un objeto que indica éxito o error, y los datos HTML generados.
  */
 export const customHTML = async ({ path, matches }) => {
     const contentHTML = new Promise((resolve, reject) => {
@@ -101,8 +108,7 @@ export const customHTML = async ({ path, matches }) => {
         })
     })
 
-    const date = new Date()
-    const dateInit = date.getMilliseconds()
+    const dateInit = new Date().getMilliseconds()
     const html = await contentHTML
     const rows = html.split('\r\n')
 
@@ -115,10 +121,10 @@ export const customHTML = async ({ path, matches }) => {
                     const startTarget = row.substring(row.indexOf('<'), row.indexOf('>') + 1)
                     const endTarget = row.substring(row.indexOf('</'), row.lastIndexOf('>') + 1)
                     const target = startTarget + match.match.value + endTarget
-                    resolve({ type: 'success', message: 'matched successfully', data: { row: target, index: index, ...match } })
+                    resolve({ type: 'success', message: 'Coincidencia exitosa', data: { row: target, index: index, ...match } })
                 }
             }
-            reject({ type: 'error', message: 'Error to match', data: { ...match } })
+            reject({ type: 'error', message: 'Error en la coincidencia', data: { ...match } })
         })
     }))
 
@@ -130,15 +136,14 @@ export const customHTML = async ({ path, matches }) => {
                     if (type == 'success') {
                         const { row, index } = data
                         rows[index] = row
-                        resolve({ type: 'success', message: 'Seted successfully' })
+                        resolve({ type: 'success', message: 'Configurado exitosamente' })
                     }
-                    reject({ type: 'error', message: 'Something was wrong...' })
+                    reject({ type: 'error', message: 'Error en la configuración' })
                 })
             }))
-                .then(solved => {
-                    const date = new Date()
-                    const dateEnd = date.getMilliseconds()
-                    resolve({ type: 'success', message: 'Seted all successfully', data: { html: rows.join('\n'), time: `${(dateEnd - dateInit)} miliseconds` } })
+                .then(() => {
+                    const dateEnd = new Date().getMilliseconds()
+                    resolve({ type: 'success', message: 'Todo configurado exitosamente', data: { html: rows.join('\n'), time: `${(dateEnd - dateInit)} milisegundos` } })
                 })
                 .catch(err => reject({ type: 'error', message: err }))
         })
